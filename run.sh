1958683dc683da820d4481ce5728b00a7e017641
Markus Noga <ml@noga.de>
2021-03-26 14:23:17 +0100

    filtering out traces/ directory itself to avoid spurious .analysis file

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #/bin/bas
-find traces/ | grep -v '\.analysis' | xargs -n1 -P `nproc` -t -r ./tcptimestamps -g -r  
+find traces/* | grep -v '\.analysis' | xargs -n1 -P `nproc` -t -r ./tcptimestamps -g -r  
 mkdir -p analyses/
 mv traces/*.analysis analyses
