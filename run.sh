2db791e9d004a037612b675619607a44b4cee4b7
Markus Noga <ml@noga.de>
2021-03-31 17:45:43 +0200

    added analysis/summary output

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 find traces/ | tail -n +2 | grep -v '\.analysis' | xargs -n1 -P `nproc` -t -r ./tcptimestamps -g -r  
 mkdir -p analyses/
 mv traces/*.analysis analyses
+grep "outlier packets" analyses/*.analysis | tee analyses/summary
