4fc70cd3ff793f003ac651bae85bcaa79ff690f2
Haris Sehic <hsehic@suse.com>
2021-04-03 15:26:19 +0200

    fix typo

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#/bin/bas
-find traces/ | tail -n +2 | grep -v '\.analysis' | xargs -n1 -P `nproc` -t -r ./tcptimestamp -g -r  
+#/bin/bash
+find traces/ | tail -n +2 | grep -v '\.analysis' | xargs -n1 -P `nproc` -t -r ./tcptimestamp -g -r
 mkdir -p analyses/
 mv traces/*.analysis analyses
 grep "outlier packets" analyses/*.analysis | tee analyses/summary
